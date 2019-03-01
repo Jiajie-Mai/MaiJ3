9cdb65c1b597b07b061d8e8592e239e7e2baa8d0
Jiajie-Mai <jmai3@stuy.edu>
2019-03-01 12:32:51 -0500

    did work

---
+++ 07_mongo/07_mongo.py
@@ -0,0 +1,13 @@
+'''
+Jiajie Mai
+SoftDev2 pd6
+K07 -- Import/Export Bank
+2019-03-05
+
+Name- dadjokes.json
+Description- A list of posts on Reddit from the /r/dadjokes subreddit 
+
+Link- https://www.reddit.com/r/dadjokes.json
+
+Import mechanism-
+'''
