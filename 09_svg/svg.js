e00b588e3c1e6dd45a44d56b4496dc5996def95c
Jiajie-Mai <jmai3@stuy.edu>
2019-03-13 12:33:02 -0400

    did things

--- 09_svg/svg.js
+++ 09_svg/svg.js
@@ -29,11 +29,26 @@ pic.addEventListener('click', function(e) {
   var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
   var x = e.offsetX
   var y = e.offsetY
+  var color = ["black","blue","red","green","white","purple","yellow","orange"]
   c.setAttribute("cx", x);
   c.setAttribute("cy", y);
-  c.setAttribute("r", "5");
-  c.setAttribute("fill", "red");
-  c.setAttribute("stroke", "black");
+  c.setAttribute("r", "10");
+  var fcolor = color[Math.round(7 * Math.random())];
+  while (fcolor == "white"){
+    fcolor = color[Math.round(7 * Math.random())];
+  }
+  var scolor = color[Math.round(7 * Math.random())];
+  while (fcolor == scolor) {
+    scolor = color[Math.round(7 * Math.random())];
+  }
+  var lcolor = color[Math.round(7 * Math.random())];
+  while (lcolor == "white"){
+    lcolor = color[Math.round(7 * Math.random())];
+  }
+  c.setAttribute("fill", fcolor);
+  c.setAttribute("stroke", scolor);
+  console.log("Fill color: " + fcolor);
+  console.log("Stroke color: " + scolor)
   console.log("Added point to (" + x + "," + y + ").")
   pic.appendChild(c);
   if (cleared == false) {
@@ -42,7 +57,7 @@ pic.addEventListener('click', function(e) {
     line.setAttribute("y1", ly);
     line.setAttribute("x2", x);
     line.setAttribute("y2", y);
-    line.setAttribute("style", "stroke: black;")
+    line.setAttribute("style", "stroke: " + lcolor);
     console.log("Added line from to (" + lx + "," + ly + ") to (" + x + "," + y + ").")
     pic.appendChild(line)
   }
