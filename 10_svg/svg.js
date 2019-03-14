3a7374facb3d45f80dff36188080410924459c26
Jiajie <33970456+Jiajie-Mai@users.noreply.github.com>
2019-03-14 01:10:57 -0400

    did hw 10

--- 10_svg/svg.js
+++ 10_svg/svg.js
@@ -1,1 +1,53 @@
-// hello
+/*
+ * Jiajie Mai
+ * SoftDev2 pd6
+ * K10: Ask Circles [Change || Die]
+ * 2019-03-13
+ */
+
+var pic = document.getElementById("vimage");
+var clear = document.getElementById("clear");
+var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+var noCircle = false;
+
+clear.addEventListener('click', function(e) {
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    c.setAttribute("x", 0);
+    c.setAttribute("y", 0);
+    c.setAttribute("width", 500);
+    c.setAttribute("height", 500);
+    c.setAttribute("fill", "white");
+    pic.appendChild(c);
+    console.log("Cleared.")
+})
+
+pic.addEventListener('click', function(e) {
+  var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  var x = e.offsetX;
+  var y = e.offsetY;
+
+  c.addEventListener('click', function(e) {
+    if (c.getAttribute("fill") == "green") {
+      c.setAttribute("fill", "blue");
+      c.setAttribute("cx", Math.round(Math.random() * 501));
+      c.setAttribute("cy", Math.round(Math.random() * 501));
+    } else {
+      c.setAttribute("fill", "green");
+    }
+    noCircle = true;
+  })
+
+  if (!noCircle) {
+    c.setAttribute("cx", x);
+    c.setAttribute("cy", y);
+    c.setAttribute("r", "10");
+    c.setAttribute("fill", "blue");
+    c.setAttribute("stroke", "black");
+  }
+
+  noCircle = false;
+
+  console.log("Added point to (" + x + "," + y + ").")
+  pic.appendChild(c);
+})
