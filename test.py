c5ee84da10d0342e64c925b385fe972786695e49
Jiajie-Mai <jmai3@stuy.edu>
2019-04-11 12:32:06 -0400

    added test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+nums = []
+for x in range(8):
+    nums.append(x)
+
+squares = []
+for x in range(8):
+    nums.append(x**2)
+
+[x for x in range(8)]
+
+def passCheck(password):
+    number = "1234567890"
+    UC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    LC = "abcdefghijklmnopqrstuvwxyz"
+    special = ".?!&#,;:-_*"
+    [x for x in password if x in UC_LETTERS]
