463733d717fe9eff58ff673d9344b65ddb44500e
Jiajie-Mai <jmai3@stuy.edu>
2019-04-15 12:32:32 -0400

    worked on 17

--- 17_listcomp/17_listcomp.py
+++ 17_listcomp/17_listcomp.py
@@ -1,26 +1,29 @@
-#Jiajie Mai
+x #Jiajie Mai
 #SoftDev2 pd6
 #K#17--PPFTLCW
 #2019-04-15
 
+#['00','22','44','66','88']
 def function1(list):
     result = []
     for x in range(5):
         result.append(str(22 * x))
     print(result)
-	result = [str(22 * x)for x in range(5)]
+	result = [str(2*x)+str(2*x) for x in range(5)]
 	print(result)
 	return result
 
+#[7,17,27,37,47]
 def function2(list):
     result = []
     for x in range(5):
         result.append(7 + 10 * x)
     print(result)
-	result = [7 + 10 * x for x in rang(5)]
+	result = [7 + 10 * x for x in range(5)]
 	print(result)
 	return result
 
+#[0,0,0,0,1,2,0,2,4]
 def function3(list):
     result = []
     for x in range(9):
@@ -35,27 +38,35 @@ def function3(list):
     print(result)
     return result
 
+#need to redo
+
+#composite numbers
 def function4():
     result = []
     for x in range(101):
-        for y in range(x):
-            if (y != 0 and y != 1 and y != x and x % y == 0)
-                result.append(x)
+       for y in range(x*2, 50, x):
+         result.append(y)
 	print (result)
-	result = [x for x in range(101) for y in range(x) if (y != 0 and y != 1 and y != x and x % y == 0)]
+	result = [x for y in range(1,101) for x in range(y*2, 50, y)]
 	print(result)
 	return result
 
+#need to redo
+
+#prime numbers
 def function5():
     result = []
     for x in range(101):
-        for y in range(x):
-            if (y != 0 and y != 1 and y != x and x % y == 0)
+       for y in range(x*2, 50, x):
+         result.append(y)
 	print (result)
-	result = [x for x in range(101) for y in range(x) if (y != 0 and y != 1 and y != x and x % y == 0)]
+	result = [x for x in range(,101) if (x != 0 and x != 1 and x not in function4())]
 	print(result)
 	return result
 
+#need to redo
+
+#finding factors for a number
 def function6(num):
     result = []
     if x for range(101):
@@ -66,7 +77,7 @@ def function6(num):
 	print(result)
     return result
 
-#need to fix
+#switching matrix
 def function7(matrix):
     result = []
     for x in range(len(matrix[0])):
@@ -74,7 +85,7 @@ def function7(matrix):
         for y in range(len(matrix)):
             result[x].append(matrix[y][x])
     print(result)
-	result = [ [ matrix[j][i] for j in range(len(matrix)) ] for i in range(len(matrix[0])) ]
+	result = [ [ matrix[x][y] for x in range(len(matrix)) ] for y in range(len(matrix[0])) ]
 	print(result)
     return result
 
