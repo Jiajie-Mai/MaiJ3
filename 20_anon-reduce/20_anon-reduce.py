bfa3d274c29581d70d8da3231466b700a4e84dc7
Jiajie <33970456+Jiajie-Mai@users.noreply.github.com>
2019-04-17 21:52:45 -0400

    changed name for folders

--- 20_anon-reduce/20_anon-reduce.py
+++ 20_anon-reduce/20_anon-reduce.py
@@ -18,4 +18,4 @@ def wordsFreq(w):
     ws = [x for x in re.split('[ \r\n]', w) if x != '']
     return reduce((lambda x,y : x + y), [1 for x in [x for x in range(len(words)) if words[x] == ws[0]] if words[x:x + len(ws)] == ws])
 
-print(wordFreq('the'))
+print(wordFreq('have'))
