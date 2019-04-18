9bd25c16006e0dc209bee2c60570ba57c0fc8059
Jiajie <33970456+Jiajie-Mai@users.noreply.github.com>
2019-04-17 21:46:08 -0400

    worked on hw 20

--- 20_listcomp/20_listcomp.py
+++ 20_listcomp/20_listcomp.py
@@ -1,8 +1,21 @@
-moby = []
+from functools import reduce
+import os
+import re
 
 with open('Moby Dick.txt','r') as f:
-    for line in f:
-        for word in line.split():
-            moby.extend(word)
+    word = f.read()
+words = [x for x in re.split('[ \r\n]', word) if x != '']
 
-print(moby)
+
+print(words)
+
+def wordFreq(w):
+    return reduce((lambda x,y : x + y), [1 for x in words if x == w])
+
+print(wordFreq('the'))
+
+def wordsFreq(w):
+    ws = [x for x in re.split('[ \r\n]', w) if x != '']
+    return reduce((lambda x,y : x + y), [1 for x in [x for x in range(len(words)) if words[x] == ws[0]] if words[x:x + len(ws)] == ws])
+
+print(wordFreq('the'))
