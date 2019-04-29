1244424e724d41da81b372b968d6ac95af4b10d0
Jiajie-Mai <jmai3@stuy.edu>
2019-04-29 12:32:16 -0400

    added work

---
+++ 21_js-mfr/21_js-mfr.js
@@ -0,0 +1,10 @@
+// Jiajie Mai
+// SoftDev2 pd6
+// K#21--Onions, Bell Peppers, and Celery, Oh My!  J
+// 2019-04-30
+
+var main = document.getElementById("main");
+
+var data = ["Hi"];
+
+console.log(data);
