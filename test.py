185678921951b95fe678b74c2df7de6bc48e7eec
Jiajie-Mai <jmai3@stuy.edu>
2019-05-01 12:32:39 -0400

    added test files'

--- test.py
+++ test.py
@@ -1,16 +1,61 @@
-nums = []
-for x in range(8):
-    nums.append(x)
+import random
+from random import randint
 
-squares = []
-for x in range(8):
-    nums.append(x**2)
+def make_HTML_heading(f):
+    txt = f()
+    def inner():
+        return '<h1>' + txt + '<h1>'
+    return inner
 
-[x for x in range(8)]
+def greet():
+    greetings = ['Hello','Welcome','AYO!','Hola','Bonjour','Word up']
+    return random.choice(greetings)
 
-def passCheck(password):
-    number = "1234567890"
-    UC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    LC = "abcdefghijklmnopqrstuvwxyz"
-    special = ".?!&#,;:-_*"
-    [x for x in password if x in UC_LETTERS]
+print("\nTesting greeting")
+greetings = make_HTML_heading(greet)
+print(greetings())
+
+del greet
+print("\nNow doing the same thing but with a decorator\n")
+
+# equiv to greet = make_HTML_heading(greet)
+@make_HTML_heading
+def greet():
+    greetings = ['Hello','Welcome','AYO!','Hola','Bonjour','Word up']
+    return random.choice(greetings)
+
+print(greet())
+print("")
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+print("Testing Fibonacci")
+i = randint(1,20)
+print(fib(i))
+print("Fibonacci number for " + str(i))
+
+# del fib
+#
+# def memoize(f):
+#     memo = {}
+#     def helper(x):
+#         ???
+#     return helper
+#
+# def fib(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fib(n-1) + fib(n-2)
+#
+# print("\nNow doing the same thing but with a memoizing\n")
+# fib = memoize(fib)
+# print(fib(10))
